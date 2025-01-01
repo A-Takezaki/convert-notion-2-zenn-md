b8ca99c312cff1df7acd4e57bd4fc4681666f3eb
You <you@example.com>
2025-01-01 13:32:49 +0900

    add comment

--- n2m.js
+++ n2m.js
@@ -4,20 +4,26 @@ import fs from 'fs';
 import dotenv from 'dotenv';
 import { resourceLimits } from "worker_threads";
 
+// Initialize
+// Load environment variables
 dotenv.config();
 const notionIntegrationToken = process.env.NOTION_INTEGRATION_TOKEN;
 const notionTargetPageId = process.env.NOTION_TARGET_PAGE_ID;
 
- 
+
 const notion = new Client({
   auth: notionIntegrationToken,
 });
 
 // passing notion client to the option
 const n2m = new NotionToMarkdown({ notionClient: notion });
+// Initialize END
 
+// Get the page
 (async () => {
   const mdblocks = await n2m.pageToMarkdown(notionTargetPageId);
   const mdString = n2m.toMarkdownString(mdblocks);
+
+
   console.log(mdString.parent);
 })();
